63ae2264b6bd5c8a7f0d5396ad89f6aecd4a8e87
Ashu Singh <ashu.ask.singh@gmail.com>
2022-11-21 16:34:58 +0530

    add react team page

--- src/main/java/com/singashi/ipldashboard/controller/TeamController.java
+++ src/main/java/com/singashi/ipldashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.singashi.ipldashboard.model.Team;
 import com.singashi.ipldashboard.repository.MatchRepository;
 import com.singashi.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
